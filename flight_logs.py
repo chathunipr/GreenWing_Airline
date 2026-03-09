a1f6df71485ddb2b34d93a094e3b8f44d211e79f
Sandali Nimalka <sandalinimalka@outlook.com>
2026-03-09 17:32:28 +0530

    update calculations in processFlight

--- flight_logs.py
+++ flight_logs.py
@@ -14,3 +14,17 @@ def processFlight(userID, routeid, profit, co2_per_flight):
     """, (userID,))
 
     result = cur.fetchone()
+
+    current_budget = float(result[0])
+    total_profit = float(result[1])
+    total_co2 = float(result[2])
+    reputation = float(result[3])
+
+    # update values
+    new_budget = current_budget + profit
+    new_total_profit = total_profit + profit
+    new_total_co2 = total_co2 + co2_per_flight
+
+    # reputation loss from pollution
+    reputation_loss = co2_per_flight * 0.25
+    new_reputation = reputation - reputation_loss
