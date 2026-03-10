1cf07cd9efcd4317eb2385f610644ae80beb85eb
dxasanka-cyber <dxasanka@gmail.com>
2026-03-10 18:37:10 +0530

    Validate user input for positive numeric values within budget - Calculate CO2 reduction and reputation increase - Update budget, total CO2, and reputation values.

--- invesments.py
+++ invesments.py
@@ -18,3 +18,35 @@ def investGreenTech(userID):
     reputation = float(result[2])
 
     print("\nYour Current Budget:", current_budget)
+    while True:
+
+        amount_input = input("Enter amount to invest in Green Tech: ")
+
+        if amount_input.isdigit():
+
+            amount = float(amount_input)
+
+            if amount <= 0:
+                print("Invalid amount. Please enter a positive value.")
+
+            elif amount > current_budget:
+                print("You do not have enough budget for this investment.")
+
+            else:
+                break
+
+        else:
+            print("Invalid input. Please enter numbers only.")
+
+        # calculate benefits
+    co2_reduced = amount / 10000
+    reputation_increased = amount / 20000
+
+    # update values
+    new_budget = current_budget - amount
+    new_total_co2 = max(0, total_co2 - co2_reduced)
+    new_reputation = reputation + reputation_increased
+
+    # prevent CO2 going below zero
+    # if new_total_co2 < 0:
+    #     new_total_co2 = 0
